cf4b7e0b27d8926bf30aac87e31739d5c8034571
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-09 16:18:44 +0300

    Update App.js
    Roundedbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { RoundedBox } from "@react-three/drei";
 
 function App() {
   return (
     <>
       <ambientLight intensity={0.1} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+      <RoundedBox
+        args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
+        radius={0.05} // Radius of the rounded corners. Default is 0.05
+        smoothness={4} // The number of curve segments. Default is 4
+      >
+        <meshPhongMaterial color="#f3f3f3" wireframe />
+      
+      </RoundedBox>
+
     </>
   );
 }
