49aa12611606538528f77c4c95733261b0699f3d
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-10 15:31:48 +0300

    Update App.js
    h1 responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { RoundedBox, ScrollControls, Scroll } from "@react-three/drei";
+import { ScrollControls, Scroll } from "@react-three/drei";
 
 
 function App() {
@@ -13,8 +13,30 @@ function App() {
 
         </Scroll>
 
-        <Scroll html>
+        <Scroll html style={{width: '100%'}}>
 
+        <h1 
+          className='title text-gray-400 text-7xl sm:text-8xl md:text-9xl lg:text-10xl xl:text-11xl flex justify-center h-screen mt-5'>
+          PHANTOM
+        </h1>
+
+          <div className='row' style={{ position: 'absolute', top: `132vh`}}>
+            <h2>Be a Man of the Future.</h2>
+            <p style={{ maxWidth: '400px' }}>Featuring a sleek, metallic design inspired by advanced technology, this aftershave bottle is as stylish as it is functional. But it's not just a pretty face - inside, you'll find a nourishing and protective aftershave formula that will leave your skin feeling refreshed and hydrated.</p>
+            <button>Read more</button>
+          </div>
+
+          <div className='row' style={{ position: 'absolute', top: `230vh`}}>
+            <div className='col' style={{ position: 'absolute', right: `40px`, width: "540px"}}>
+              <h2 style={{ maxWidth: "440px" }}>Tech-Savvy Side</h2>
+              <p style={{ maxWidth: '440px' }}>Featuring a sleek, metallic design inspired by advanced technology, this aftershave bottle is as stylish as it is functional. But it's not just a pretty face - inside, you'll find a nourishing and protective aftershave formula that will leave your skin feeling refreshed and hydrated.</p>                
+              <button>Read more</button>
+            </div>
+          </div>
+          
+          <h2 style={{ position: 'absolute', top: '350vh', left: '50%', transform: `translate(-50%,-50%)` }}>Cutting-Edge of Grooming</h2>              
+          
+          <button style={{ position: 'absolute', top: `590vh`,left: '50%', transform: `translate(-50%,-50%)` }}>Buy now</button>
         </Scroll>
 
       </ScrollControls>
