1ae9aedb20c99b2e3e9651c82725ce01aab49f2e
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-10 14:22:55 +0300

    Update App.js
    scroll horizontal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
       <color attach="background" args={["#333333"]} />
 
-      <ScrollControls pages={3} damping={0.1}>
+      <ScrollControls pages={3} damping={0.1} horizontal>
 
         <ambientLight intensity={0.5}/>
         <RoundedBox
@@ -25,8 +25,8 @@ function App() {
         <Scroll html>
           {/* DOM contents in here will scroll along */}
           <h1>html in here (optional)</h1>
-          <h1 style={{position:'absolute', top: '100vh' }}>second page</h1>
-          <h1 style={{ position:'absolute', top: '200vh' }}>third page</h1>
+          <h1 style={{position:'absolute', left: '100vw' }}>second page</h1>
+          <h1 style={{ position:'absolute', left: '200vw' }}>third page</h1>
         </Scroll>
       </ScrollControls>
 
