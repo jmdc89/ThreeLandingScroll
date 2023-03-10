88f55ce2afeeef8df7e1b5ecb11f5f427a949028
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-10 15:15:38 +0300

    Update App.js
    cleaning

--- src/App.js
+++ src/App.js
@@ -7,27 +7,16 @@ function App() {
     <>
       <color attach="background" args={["#333333"]} />
 
-      <ScrollControls pages={3} damping={0.1} horizontal>
+      <ScrollControls pages={3} damping={0.1}>
 
-        <ambientLight intensity={0.5}/>
-        <RoundedBox
-          args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
-          radius={0.05} // Radius of the rounded corners. Default is 0.05
-          smoothness={4} // The number of curve segments. Default is 4
-        >
-          <meshPhongMaterial color="red"/>
-        </RoundedBox>
-        
         <Scroll>
 
         </Scroll>
 
         <Scroll html>
-          {/* DOM contents in here will scroll along */}
-          <h1>html in here (optional)</h1>
-          <h1 style={{position:'absolute', left: '100vw' }}>second page</h1>
-          <h1 style={{ position:'absolute', left: '200vw' }}>third page</h1>
+
         </Scroll>
+
       </ScrollControls>
 
     </>
