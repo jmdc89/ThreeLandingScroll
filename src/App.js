cca149b32114c13f48e0cb30384862e6aea631ea
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-10 13:51:06 +0300

    Update App.js
    useFrame test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { RoundedBox } from "@react-three/drei";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 function App() {
+
+  const test = useRef()
+
+  useFrame((state,delta) => {
+      //console.log(test)
+  })
+
   return (
     <>
       <color attach="background" args={["#333333"]} />
@@ -9,6 +18,7 @@ function App() {
         args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
         radius={0.05} // Radius of the rounded corners. Default is 0.05
         smoothness={4} // The number of curve segments. Default is 4
+        ref={test}
       >
         <meshPhongMaterial color="red" />
       
