24f1384eb7b3737540fbc03bee668f7c1cde0dad
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-10 14:14:29 +0300

    Update App.js
    Scroll fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { ScrollControls, Scroll } from "@react-three/drei";
+import { RoundedBox, ScrollControls, Scroll } from "@react-three/drei";
 
 
 function App() {
@@ -8,13 +8,25 @@ function App() {
       <color attach="background" args={["#333333"]} />
 
       <ScrollControls pages={3} damping={0.1}>
+
+        <ambientLight intensity={0.5}/>
+        <RoundedBox
+          args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
+          radius={0.05} // Radius of the rounded corners. Default is 0.05
+          smoothness={4} // The number of curve segments. Default is 4
+        >
+          <meshPhongMaterial color="red"/>
+        </RoundedBox>
+        
         <Scroll>
+
         </Scroll>
+
         <Scroll html>
           {/* DOM contents in here will scroll along */}
           <h1>html in here (optional)</h1>
-          <h1 style={{ top: '100vh' }}>second page</h1>
-          <h1 style={{ top: '200vh' }}>third page</h1>
+          <h1 style={{position:'absolute', top: '100vh' }}>second page</h1>
+          <h1 style={{ position:'absolute', top: '200vh' }}>third page</h1>
         </Scroll>
       </ScrollControls>
 
