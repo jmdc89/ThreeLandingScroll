95feb207c683e3b6013ec2c96ea9db889b878e85
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-10 14:03:10 +0300

    Update App.js
    scrollControls

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import { RoundedBox } from "@react-three/drei";
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { ScrollControls, Scroll } from "@react-three/drei";
+
 
 function App() {
-
-  const test = useRef()
-
-  useFrame((state,delta) => {
-      //console.log(test)
-
-      test.current.rotation.y += delta
-  })
 
   return (
     <>
       <color attach="background" args={["#333333"]} />
-      <ambientLight intensity={0.5} />
-      <RoundedBox
-        args={[1, 1, 1]} // Width, height, depth. Default is [1, 1, 1]
-        radius={0.05} // Radius of the rounded corners. Default is 0.05
-        smoothness={4} // The number of curve segments. Default is 4
-        ref={test}
-      >
-        <meshPhongMaterial color="red" />
-      
-      </RoundedBox>
+
+      <ScrollControls pages={3} damping={0.1}>
+        <Scroll>
+        </Scroll>
+        <Scroll html>
+          {/* DOM contents in here will scroll along */}
+          <h1>html in here (optional)</h1>
+          <h1 style={{ top: '100vh' }}>second page</h1>
+          <h1 style={{ top: '200vh' }}>third page</h1>
+        </Scroll>
+      </ScrollControls>
 
     </>
   );
